9de48339cdaa76e6264199aaa8ccf486a6a8eea3
nadcdc <nadcdc4@gmail.com>
2018-10-31 21:38:07 +0900

    production용 Dockerfile, settings, wsgi, .config작성

---
+++ app/config/settings/production.py
@@ -0,0 +1,16 @@
+
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = []
+
+WSGI_APPLICATION = 'config.wsgi.production.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_URL = '/static/'
